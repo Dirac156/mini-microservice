04ded07979b0346d4a5b122f07a08be6244f09b4
Dirac Murairi <dmurairimukongya@gmail.com>
2022-08-24 10:05:44 +0200

    create a new query service and implement functionnalities

--- query/index.js
+++ query/index.js
@@ -6,8 +6,43 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/posts", (req, res) => {
+const posts = {};
 
+/*
+posts === {
+    'j123j43': {
+        id: 'j123j43',
+        title: 'post title',
+        comments: [
+            { id: 'ds3r32d', content: 'comment' }
+        ]
+    }
+}
+*/
+
+app.get("/posts", (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    const { id } = data;
+
+    switch (type) {
+        case "PostCreated":
+            const { title } =  data;
+            posts[id] = { id, title, comments: []}
+            break;
+        case "CommentCreated":
+            const { content, postId } = data;
+            const post = posts[postId];
+            post.comments.push({ id, content });
+            break;
+        default:
+            break;
+    }
+    console.log(posts)
+    res.send({})
 })
 
 app.post("/posts", (req, res) => {
